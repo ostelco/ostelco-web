6fee4ffa3320d72d45cc5c558af7ec100174c3e2
Henrik Holmlund <henrikholmlund@henriks-mbp.guest.stockholm.se>
2018-11-29 14:54:49 +0100

    fixed user states plus weird input default styling, also sert up right api call to mailchimp

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -25,48 +25,48 @@ function validate_input(email) {
   return re.test(email);
 }
 
-$(document).ready(function() {
-  // I only have one form on the page but you can be more specific if need be.
-  var $form = $('form');
-  console.log($form);
+// $(document).ready(function() {
+//   // I only have one form on the page but you can be more specific if need be.
+//   var $form = $('form');
+//   console.log($form);
 
-  if ($form.length > 0) {
-    $('form input[type="submit"]').bind('click', function(event) {
-      console.log('i clicked something', $form[0][0].value);
-      console.log('this is the form action url', $form.attr('action'));
-      if (event) event.preventDefault();
-      // validate_input() is a validation function I wrote, you'll have to substitute this with your own.
-      if (validate_input($form)) {
-        register($form);
-      }
-    });
-  }
-});
+//   if ($form.length > 0) {
+//     $('form input[type="submit"]').bind('click', function(event) {
+//       console.log('i clicked something', $form[0][0].value);
+//       console.log('this is the form action url', $form.attr('action'));
+//       if (event) event.preventDefault();
+//       // validate_input() is a validation function I wrote, you'll have to substitute this with your own.
+//       if (validate_input($form)) {
+//         register($form);
+//       }
+//     });
+//   }
+// });
 
-function register($form) {
-  $.ajax({
-    type: $form.attr('method'),
-    url: $form.attr('action'),
-    data: $form.serialize(),
-    cache: false,
-    dataType: 'json',
-    contentType: 'application/json; charset=utf-8',
-    error: function(err) {
-      alert(
-        'Could not connect to the registration server. Please try again later.'
-      );
-    },
-    success: function(data) {
-      if (data.result != 'success') {
-        // Something went wrong, do something to notify the user. maybe alert(data.msg);
-        console.log(data.msg);
-      } else {
-        // It worked, carry on...
-        console.log('it worked', data);
-      }
-    }
-  });
-}
+// function register($form) {
+//   $.ajax({
+//     type: $form.attr('method'),
+//     url: $form.attr('action'),
+//     data: $form.serialize(),
+//     cache: false,
+//     dataType: 'json',
+//     contentType: 'application/json; charset=utf-8',
+//     error: function(err) {
+//       alert(
+//         'Could not connect to the registration server. Please try again later.'
+//       );
+//     },
+//     success: function(data) {
+//       if (data.result != 'success') {
+//         // Something went wrong, do something to notify the user. maybe alert(data.msg);
+//         console.log(data.msg);
+//       } else {
+//         // It worked, carry on...
+//         console.log('it worked', data);
+//       }
+//     }
+//   });
+// }
 
 // Select all links with hashes
 $('a[href*="#"]')
